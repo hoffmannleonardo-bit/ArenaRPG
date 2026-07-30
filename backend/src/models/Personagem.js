315ea63ee3656136448927e37834e28fa479eddf
hoffmannleonardo-bit <hoffmann.leonardo@escola.pr.gov.br>
2026-07-30 20:28:24 +0000

    criaçao da classe personagem

--- backend/src/models/Personagem.js
+++ backend/src/models/Personagem.js
@@ -10,27 +10,51 @@
 export class Personagem {
   // PASSO 2 — Crie o constructor com os atributos do personagem.
   //           Parâmetros: nome, vida, ataque, defesa, emoji (padrão "🧍")
+  constructor(nome, vida, ataque, defesa, defesa, emoji = "🧍"){
+    this.nome= nome;
+    this.vida= vida;
+    this.vidaMax= vidaMax;
+    this.ataque= ataque;
+    this.defesa= defesa;
+    this.emoji= emoji;
+    this.classe= "personagem";
+  }
 
 
   // PASSO 3 — Método estaVivo(): retorna true se this.vida > 0.
+  estavivo() {
+    return this.vida >0;
+  }
 
 
 
   // PASSO 4 — Método receberDano(dano):
   //   const danoReal = Math.max(1, dano - this.defesa)
   //   diminua this.vida (sem passar de 0) e retorne danoReal.
+  receberDano(dano) {
+    const danoReal = Math.max(1, dano - this.defesa)
+     this.vida= math.max( 0, this.vida - danoReal);
+     return danoReal;
+  }
 
 
 
   // PASSO 5 — Método atacar(alvo):
   //   chame alvo.receberDano(this.ataque), guarde em danoReal
   //   e retorne uma frase para o log, ex:
-  //   `${this.emoji} ${this.nome} atacou ${alvo.nome} causando ${danoReal} de dano!`
+  //   `${this.emoji} ${this.nome} atacou ${alvo.nome} causando ${danoReal} de dano!`]
+  atacar(alvo) {
+    const danoReal= alvo.receberDano(this.ataque);
+    return `${this.emoji} ${this.nome} atacou ${alvo.nome} causando ${danoReal} de dano!`;
+  }
 
 
 
   // PASSO 6 — Método resetar(): devolve this.vida = this.vidaMax
   //   (usado para "curar" antes de cada nova batalha)
+  resetar() {
+    this.vida = this.vidaMax;
+  }
 
 
 
@@ -39,6 +63,17 @@ export class Personagem {
   //   Retorne personagens.map((p, indice) => ({ ... }))
   //   com: id: indice, nome, classe, vida: p.vidaMax, ataque, defesa, emoji.
   //   Obs: método estático é chamado por  Personagem.paraLista(...)
+  static paraLista(personagens) {
+    return personagens.map((p, indice)=> ({
+      id: indice,
+      nome: p. nome,
+      classe: p. classe,
+      vida: p.vidaMax,
+      ataque: p.ataque,
+      defesa: p. defesa,
+      emoji:p. emoji,
+    }));
+  }
 
 
 }
